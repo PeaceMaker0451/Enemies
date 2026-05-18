1d57805af1ab19a40a03f75e3a76446f916fb73b
peacemkr_png <tyumentsev.png@gmail.com>
2026-05-18 21:19:36 +0900

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed, Space.Self);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private Enemy _prefab;
+        [SerializeField] private Transform[] _spawnPoints;
+        [SerializeField] private float _spawnDelay = 2f;
+        
+        private void Start()
+        {
+            StartCoroutine(SpawnCycle());
+        }
+
+        private void Spawn()
+        {
+            int randomSpawnPointIndex = Random.Range(0, _spawnPoints.Length);
+            Transform spawnPoint = _spawnPoints[randomSpawnPointIndex];
+            
+            var enemy = Instantiate(_prefab);
+            enemy.transform.position = spawnPoint.position;
+            enemy.transform.rotation = spawnPoint.rotation;
+        }
+
+        private IEnumerator SpawnCycle()
+        {
+            bool shouldSpawn = true;
+            var wait = new WaitForSeconds(_spawnDelay);
+
+            while (shouldSpawn)
+            {
+                yield return wait;
+                Spawn();
+            }
+        }
+    }
+}
